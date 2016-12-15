ad39f6a833a61789e59f56f8f74575219c8e9383
almas-alright <kutsnalmas@gmail.com>
2016-12-15 19:22:29 +0600

    d

--- src/image/lock/DisplayGraphics.java
+++ src/image/lock/DisplayGraphics.java
@@ -27,13 +27,5 @@ public class DisplayGraphics extends Canvas {
 
     }
 
-    public static void main(String[] args) {
-        DisplayGraphics m = new DisplayGraphics();
-        JFrame f = new JFrame();
-        f.add(m);
-        f.setSize(400, 400);
-        //f.setLayout(null);  
-        f.setVisible(true);
-    }
 
 }
--- src/image/lock/ImageLock.java
+++ src/image/lock/ImageLock.java
@@ -7,6 +7,8 @@ package image.lock;
 
 import java.awt.*;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 /**
  *
@@ -19,12 +21,22 @@ public class ImageLock {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        DisplayGraphics m = new DisplayGraphics();
-        JFrame f = new JFrame();
-        f.add(m);
-        f.setSize(400, 400);
-        //f.setLayout(null);  
-        f.setVisible(true);
+//        DisplayGraphics m = new DisplayGraphics();
+//        JFrame f = new JFrame();
+//        f.add(m);
+//        f.setSize(400, 400);
+//        //f.setLayout(null);  
+//        f.setVisible(true);
+
+        FileChooserDemo fd = new FileChooserDemo();
+        
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                //Turn off metal's use of bold fonts
+                UIManager.put("swing.boldMetal", Boolean.FALSE); 
+                fd.createAndShowGUI();
+            }
+        });
 
     }
 
